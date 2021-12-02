cb4935c7481f59bb1f3e5525abd40551ffa4c601
Solène Duprat <solene.duprat@gmail.com>
2021-12-02 00:29:37 -0500

    Day 2

--- Day-2/part1_solution.rb
+++ Day-2/part1_solution.rb
@@ -1,1 +1,43 @@
 # Advent of Code Day 2 - Part 1 Solution.
+
+# Create a Hash called POS
+POS = {
+  x: 0,
+  y: 0
+}
+
+# Create a method to move submarine:
+
+def move_submarine(place)
+  # Split the 'place' string where there is a space (turns into an array like ["up", "1"])
+  array = place.split
+
+  # Direction is the first element of array
+  direction = array[0]
+
+  # Coord is the second element. Convert it to an integer.
+  coord = array[1].to_i
+
+  # Reassign the values inside the POS hash depending on value of direction:
+
+  case direction
+  when 'forward' then POS[:x] = POS[:x] + coord
+  when 'up' then POS[:y] = POS[:y] - coord
+  when 'down' then POS[:y] = POS[:y] + coord
+  end
+end
+
+# Read the txt file for puzzle input
+file = File.open("puzzle_input.txt")
+
+# Store positions in array
+pos_array = file.readlines.map { |pos| pos.chomp }
+
+# For each position, move submarine
+pos_array.each do |place|
+  move_submarine(place)
+end
+
+# Display solution in the terminal
+print "Solution is: "
+puts POS[:x] * POS[:y]
--- Day-2/part2_solution.rb
+++ Day-2/part2_solution.rb
@@ -1,1 +1,32 @@
 # Advent of Code Day 2 - Part 2 Solution.
+
+POS = {
+  x: 0,
+  y: 0,
+  aim: 0
+}
+
+def move_submarine(place)
+  array = place.split
+
+  direction = array[0]
+  coord = array[1].to_i
+
+  case direction
+  when 'up' then POS[:aim] = POS[:aim] - coord
+  when 'down' then POS[:aim] = POS[:aim] + coord
+  when 'forward'
+    POS[:x] = POS[:x] + coord
+    POS[:y] = POS[:y] + (coord * POS[:aim])
+  end
+end
+
+file = File.open("puzzle_input.txt")
+pos_array = file.readlines.map { |pos| pos.chomp }
+
+pos_array.each do |place|
+  move_submarine(place)
+end
+
+print "Solution is: "
+puts POS[:x] * POS[:y]
